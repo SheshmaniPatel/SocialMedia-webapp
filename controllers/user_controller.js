12a9a32cdda69a8bc32e42cd42aaf003431ef20f
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-04-19 00:19:39 +0530

    succesfully added signUp page and call database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,33 +1,53 @@
-const { response } = require("express");
+const User = require("../models/user");
+ 
 
-module.exports.profile=(request,response)=>{
- return response.render('user',{
-    title:"user"
- });
-
+module.exports.profile = (request, response) => {
+  return response.render("user", {
+    title: "user",
+  });
 };
 
 // render the sign_Up page
-module.exports.signUp=(request,response)=>{
-   return response.render('user_sign_up',{
-      title:"Codeal | Sign Up"
-   })
-}
+module.exports.signUp = (request, response) => {
+  return response.render("user_sign_up", {
+    title: "Codeal | Sign Up",
+  });
+};
 
 // render the sign_In page
-module.exports.signIn=(request,response)=>{
-   return response.render('user_sign_in',{
-      title:"Codeal | Sign In"
-   })
-}
+module.exports.signIn = (request, response) => {
+  return response.render("user_sign_in", {
+    title: "Codeal | Sign In",
+  });
+};
 
 //get the sign-Up data
-module.exports.create=(request,response)=>{
-// todo later
+module.exports.create = (request, response) => {
+  if (request.body.password != request.body.confirm_password) {
+    return response.redirect('back');
+  }
 
-}
+  User.findOne({email:request.body.email},function(err, user) {
+    if (err) {
+      console.log("Error in finding user in Singing up");
+      return;
+    }
+
+    if (!user) {
+      User.create(request.body, function(err, user) {
+        if (err) {
+          console.log("Error in finding user in Singing up");
+          return;
+        }
+        return response.redirect("/users/sign-in");
+      });
+    } else {
+      return response.redirect("back");
+    }
+  });
+};
 
 // sign-In and create the session for user
-module.exports.createsession=(request,response)=>{
-// todo later
-}
+module.exports.createsession = (request, response) => {
+  // todo later
+};
