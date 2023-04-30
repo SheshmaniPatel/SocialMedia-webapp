c2e492a1303e478523e202525794e615948a072b
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-01 04:51:07 +0530

    Passing condition in both post.js in routs and home.ejs in views

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 
 const post_controller=require('../controllers/post_controller');
 
- router.post('/create',post_controller.create);
+ router.post('/create',passport.checkAuthentication,post_controller.create);
 
 module.exports=router;
