73956df9f434ba78a8420bb1c9f83e36507acd56
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-03 01:29:44 +0530

    Adding a destroy controller

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,15 +1,33 @@
- const post=require('../models/post');
+const Post = require("../models/post");
+const Comment = require("../models/comment");
 
- module.exports.create=function(request,response){
-    post.create({
-        content:request.body.content,
-        user:request.user._id
-    },function(err,Post){
-        if(err){
-            console.log('Error while posting.....');
-            return;
-        }
-        return response.redirect('back');
-    });
+module.exports.create = function (request, response) {
+  Post.create(
+    {
+      content: request.body.content,
+      user: request.user._id,
+    },
+    function (err, Post) {
+      if (err) {
+        console.log("Error while posting.....");
+        return;
+      }
+      return response.redirect("back");
+    }
+  );
+};
 
- }
+module.exports.destroy = (request, response) => {
+  Post.findById(request.params.id, (err, post) => {
+    // .id means converting the object id to String
+    if (post.user == request.user.id) {
+      post.remove();
+
+      Comment.deleteMany({ post: request.params.id }, (err) => {
+        return response.redirect("back");
+      });
+    } else {
+      return response.redirect("back");
+    }
+  });
+};
