aa83014854fb02ea69a7f7d43d5a41cecb2495f1
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-02 16:13:13 +0530

    JUst formatting

--- models/comment.js
+++ models/comment.js
@@ -1,21 +1,24 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const commentSchema=new mongoose.Schema({
-  content:{
-    type:String,
-    required:true
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
   },
-  user:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'User'
-  },
-  post:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Post'
+  {
+    timestamps: true,
   }
-},{
-timestamps:true
-});
+);
 
-const comment=mongoose.model('comment',commentSchema);
-module.exports=comment;
+const comment = mongoose.model("comment", commentSchema);
+module.exports = comment;
