ef6a1c87dfb5254ff8a52e5ce139323034c942e8
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-03 02:55:48 +0530

    Adding a controller and checks for deleting comments related to users

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -8,7 +8,7 @@ module.exports.create = (request, response) => {
         {
           content: request.body.content,
           post: request.body.post,
-          user: request.user._id
+          user: request.user._id,
         },
         (err, comment) => {
           //error handling
@@ -18,6 +18,26 @@ module.exports.create = (request, response) => {
           response.redirect("/");
         }
       );
+    }
+  });
+};
+
+module.exports.destroy = (request, response) => {
+  Comment.findById(request.params.id, (err, comment) => {
+    if (comment.user == request.user.id) {
+      let postId = comment.post;
+
+      comment.remove();
+
+      Post.findByIdAndUpdate(
+        postId,
+        { $pull: { comments: request.params.id } },
+        (err, post) => {
+          return response.redirect("back");
+        }
+      );
+    } else {
+      return response.redirect("back");
     }
   });
 };
