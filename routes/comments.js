f8476fbf863922f1904eac40f407637094eaf7e6
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-03 02:56:53 +0530

    Adding a route for destroy/delete

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,18 @@
-const express=require('express');
-const router=express.Router();
-const passport=require('passport');
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
 
-const comments_controller=require('../controllers/comment_controller');
+const comments_controller = require("../controllers/comment_controller");
 
- router.post('/create',passport.checkAuthentication,comments_controller.create);
+router.post(
+  "/create",
+  passport.checkAuthentication,
+  comments_controller.create
+);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  comments_controller.destroy
+);
 
-module.exports=router;
+module.exports = router;
