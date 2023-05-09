bc5b6649a03df34dbbea995750b530a36af2ed14
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-09 18:36:51 +0530

    Nothing serious

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express();
 const port = 7000;
 const expresslayout = require("express-ejs-layouts");
 const db = require("./config/mongoose");
-const x=11;
-const u=11;
+ 
 //used for session cookie
 
 const session = require("express-session");
