e29864dd6b5cc766d00bba0d5637c5ae5a68c22c
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-11 02:50:48 +0530

    Adding noty and looping over the existing post on the page  and calling home_post_comment

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,9 +12,20 @@
         url: "/posts/create",
         data: newPostForm.serialize(),
         success: (data) => {
-        let newPost=newPostDom(data.data.post);
-        $('#posts-list-container>ul').prepend(newPost);
-        deletePost($(" .delete-post-button",newPost));
+          let newPost = newPostDom(data.data.post);
+          $("#posts-list-container>ul").prepend(newPost);
+          deletePost($(" .delete-post-button", newPost));
+
+          // call the create comment class
+          new PostComments(data.data.post._id);
+
+          new Noty({
+            theme: "relax",
+            text: "Post published!",
+            type: "success",
+            layout: "topRight",
+            timeout: 1500,
+          }).show();
         },
         error: (err) => {
           console.log(err.responseText);
@@ -31,7 +42,7 @@
                 <a class="delete-post-button" href="/posts/destroy/${post._id}">&#9249</a>
               </small>
         
-              ${post.content }
+              ${post.content}
               <br />
               <small> 
               ${post.user.name}
@@ -46,14 +57,14 @@
                   placeholder="Type Here to add comment..."
                   required
                 />
-                <input type="hidden" name="post" value="${post._id }" />
+                <input type="hidden" name="post" value="${post._id}" />
                 <input type="submit" value="Add Comment" />
               </form>
         
              
         
               <div class="post-comments-list">
-                <ul id="post-comments-${ post._id }">
+                <ul id="post-comments-${post._id}">
                   
                 </ul>
               </div>
@@ -61,24 +72,44 @@
           </li>`);
   };
 
-
   // Method to delete the post from DOM
-    let deletePost=(deleteLink)=>{
-        $(deleteLink).click((e)=>{
-            e.preventDefault();
+  let deletePost = (deleteLink) => {
+    $(deleteLink).click((e) => {
+      e.preventDefault();
 
-            $.ajax({
-                type:"get",
-                url:$(deleteLink).prop("href"),
-                success:(data)=>{
-                    $(`#post-${data.data.post_id}`).remove();
-                    
-                },error:(err)=>{
-                    console.log(err.responseText);
-                }
-            })
-        })
-    }
+      $.ajax({
+        type: "get",
+        url: $(deleteLink).prop("href"),
+        success: (data) => {
+          $(`#post-${data.data.post_id}`).remove();
+          new Noty({
+            theme: "relax",
+            text: "Post Deleted",
+            type: "success",
+            layout: "topRight",
+            timeout: 1500,
+          }).show();
+        },
+        error: (err) => {
+          console.log(err.responseText);
+        },
+      });
+    });
+  };
+
+  // loop over all the existing posts on the page (when the window loads for the first time) and call the delete post method on delete link of each, also add AJAX (using the class we've created) to the delete button of each
+  let convertPostsToAjax = function () {
+    $("#posts-list-container>ul>li").each(function () {
+      let self = $(this);
+      let deleteButton = $(" .delete-post-button", self);
+      deletePost(deleteButton);
+
+      // get the post's id by splitting the id attribute
+      let postId = self.prop("id").split("-")[1];
+      new PostComments(postId);
+    });
+  };
 
   createPost();
+  convertPostsToAjax();
 }
