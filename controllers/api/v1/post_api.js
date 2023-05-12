a735a173cd30dcb01ecddb5b7f218cd353a4ada3
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-13 04:08:23 +0530

    Adding a destroy section and updating index

--- controllers/api/v1/post_api.js
+++ controllers/api/v1/post_api.js
@@ -1,6 +1,42 @@
- module.exports.index=(request,response)=>{
-    return response.json(200, {
-        massage:"List of Posts",
-        posts:[]
-    })
-}
+const Post = require("../../../models/post");
+const Comment=require("../../../models/comment");
+
+module.exports.index = async (request, response) => {
+  let posts = await Post.find({})
+    .sort("-createdAt")
+    .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    });
+  return response.status(200).json({
+    massage: "List of Posts",
+    posts: posts,
+  });
+};
+
+module.exports.destroy = async (request, response) => {
+  try {
+    let post = await Post.findById(request.params.id);
+    // .id means converting the object id to String
+    //   if (post.user == request.user.id) {
+    post.remove();
+
+    await Comment.deleteMany({ post: request.params.id });
+
+    return response.status(200).json({
+      massage: "Post and associated comments deleted",
+    });
+    //   } else {
+    //     request.flash("error", "You can't delete this post !!");
+    //     return response.redirect("back");
+    //   }
+  } catch (error) {
+    console.log("***********",error);
+    return response.status(500).json({
+      massage: "Internal server error",
+    });
+  }
+};
