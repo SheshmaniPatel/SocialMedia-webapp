93daf0bb2faba870c2df7f8fbd0675d476db0434
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-05-19 11:52:55 +0530

    Making a stratgy

---
+++ config/passport_jwt_stratgy.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const JWTstrategy = require("passport-jwt").Strategy;
+const extractJWT = require("passport-jwt").ExtractJwt;
+
+const User = require("../models/user");
+
+let opts = {
+  jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken,
+  secretOrKey: "Codiel",
+};
+
+passport.use(
+  new JWTstrategy(opts, function (jwtPayload, done) {
+    User.findByIdAndDelete(jwtPayload._id, function (err, user) {
+      if (err) {
+        console.log("error while finding ");
+        return;
+      }
+
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
+
+
+module.exports=passport;
